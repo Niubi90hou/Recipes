9efd211c6080ed37cbee19b6cd8609d2c8cc9232
Yüksel Toprak <yuekseltoprak@gmail.com>
2021-02-27 21:56:17 +0100

    Initial Commit

---
+++ recipe list app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recipe list app
+//
+//  Created by Yüksel Toprak on 27.02.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recipe list app/recipe_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_list_appApp.swift
+//  recipe list app
+//
+//  Created by Yüksel Toprak on 27.02.21.
+//
+
+import SwiftUI
+
+@main
+struct recipe_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
